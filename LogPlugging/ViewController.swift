f43486ccbc607914b6d1300eb7f24ae79d526770
blashadow <blackzerogamer@gmail.com>
2020-03-11 19:08:23 -0400

    Initial Commit

---
+++ LogPlugging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LogPlugging
+//
+//  Created by Luis Romero on 3/11/20.
+//  Copyright © 2020 Luis Romero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
